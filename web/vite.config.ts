e54546770d6c4624984720645cec68ed61a22b7a
kvarela <karim@metalink.com>
2026-04-23 21:35:28 -0700

    optimizeDeps

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@brightwheel/shared'],
+  },
   server: {
     port: 5173,
     proxy: {
